1bed6b8b3b6c043963dc2570bdee79c629219d50
Zhu <1196051198@qq.com>
2020-05-30 11:06:27 +0800

    Initial Commit

---
+++ C homework/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  C homework
+//
+//  Created by cherry-lily on 2020/5/30.
+//  Copyright © 2020 cherry-lily. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
